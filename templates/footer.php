18541b7749c3d5c924d7c2d23735baedbb58c247
Roger C Guilherme <rogercguilherme@gmail.com>
2025-08-11 16:44:29 -0300

    Adiciona informação de versão ao rodapé.

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,9 @@
 </main>
-<footer>
-    <p>Criado por Roger C. Guilherme com PHP e JSON - <?php echo date('Y'); ?></p>
-</footer>
 </div>
+<footer class="container-fluid">
+    <p>Criado por <a href="https://github.com/rogercgui/gr-librogr" target="_blank">Roger C. Guilherme</a> com PHP e JSON - <?php echo date('Y'); ?> - v1.0.0</p>
+</footer>
+
 </body>
 
 </html>
